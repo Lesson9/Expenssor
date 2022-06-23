93ac89e1524c68ff5d52ff1b8855a50c75cc3776
Xiao Han <outsidenewcastle@gmail.com>
2022-06-23 15:30:00 -0700

    Create grid structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,5 +2,11 @@ import React from 'react';
 import Overview from './OverviewSection/Overview.jsx';
 
 export default function App() {
-  return (<Overview />);
+  return (
+    <div className="container">
+      <div>Money Book</div>
+      <Overview />
+      <div>Add Transaction</div>
+    </div>
+  );
 }
