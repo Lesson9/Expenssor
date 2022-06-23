5a15382020c01c41b830f47bd41503f3234759ef
Xiao Han <outsidenewcastle@gmail.com>
2022-06-23 15:13:30 -0700

    Add icons for transactions

--- client/src/components/OverviewSection/TransactionListEntry.jsx
+++ client/src/components/OverviewSection/TransactionListEntry.jsx
@@ -8,8 +8,13 @@ export default function TransactionListEntry({ data: { type, date, category, not
     <li className="transaction-list-entry">
       <div className="transaction-date">{formattedDate}</div>
       <div className="transaction-info">
-        <p className="transaction-category">{category}</p>
-        {note && <p className="transaction-note">{note}</p>}
+        <div className="transaction-icon" style={{ backgroundColor: type === 'income' ? '#c2255c' : '#2b8a3e' }}>
+          { type === 'income' ? <span class="icon material-symbols-outlined">payments</span> : <span class="icon material-symbols-outlined">shopping_cart</span> }
+        </div>
+        <div className="transaction-text">
+          <p className="transaction-category">{category}</p>
+          {note && <p className="transaction-note">{note}</p>}
+        </div>
       </div>
       { type === 'income' ? <div className="transaction-amount" style={{color: '#c2255c'}}>$ {amount}</div> : <div className="transaction-amount" style={{color: '#2b8a3e'}}>$ {amount}</div> }
     </li>
